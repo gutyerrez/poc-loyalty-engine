1587be85960676db6110b08a3193f2a98d343874
Vinícius Gutierrez da Silva Rocha <srgutyerrez@gmail.com>
2022-12-19 00:31:09 -0300

    chore: first commit

---
+++ core/Application.js
@@ -0,0 +1,13 @@
+/// <reference path="./@types/application.d.ts" />
+
+class Application {
+  constructor() {
+    this.onEnable()
+  }
+
+  onEnable() {
+    // TODO: later
+  }
+}
+
+module.exports = { Application }
---
+++ inhabitants/InhabitantsApplication.js
@@ -0,0 +1,9 @@
+import { Application } from 'core/Application.js'
+
+class InhabitantsApplication extends Application {
+  onEnable() {
+    console.log('opa')
+  }
+}
+
+module.exports = InhabitantsApplication
